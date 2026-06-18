798918283837d32b59a1b2d10c77ea2fb326c39c
jesstytam <j.tam@unsw.edu.au>
2026-06-18 14:51:53 +1000

    first commit

---
+++ db/init.sql
@@ -0,0 +1,15 @@
+--create table
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    status VARCHAR(255)
+);
+
+--populate table
+INSERT INTO books (title, author, status)
+VALUES ("For the Emperor", "Sandy Mitchell", "read"),
+("The Hobbit", "J. R. R. Tolkien", "read"),
+("The Bell Jar", "Sylvia Plath", "read"),
+("Dracula", "Bram Stoker", "reading"),
+("The Master and Margarita", "Mikhail Bulgakov", "to be read");
